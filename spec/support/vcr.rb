5f97a4e0b978f4ab7ce4c0e6bc394ee200a6c9da
Diego Henrique Domingues <diego.domingues16@gmail.com>
2015-05-21 09:02:56 -0300

    Adding Code Climate to ignore hosts

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -4,5 +4,5 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   c.hook_into :webmock
   c.configure_rspec_metadata!
-  c.ignore_hosts '127.0.0.1', 'localhost'
+  c.ignore_hosts '127.0.0.1', 'localhost', 'codeclimate.com'
 end
